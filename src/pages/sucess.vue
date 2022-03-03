05eebad4c98a2aafaf2a9ba235fcd66e318a3881
Charlotte-Rhaegal <92514617+Charlotte-Rhaegal@users.noreply.github.com>
2022-03-03 14:02:10 +0100

    query creation + home

--- src/pages/sucess.vue
+++ src/pages/sucess.vue
@@ -43,6 +43,86 @@
         </section>
       </section>
     </a>
+    <a id="mousquetaires">
+      <section>
+        <article class="orange-container">
+          <div id="orange-back">
+            <img src="../assets/image/les-mousquetaires.png" alt="logo-fraser" />
+          </div>
+        </article>
+        <section class="border-purple">
+          <article class="white-content">
+            <div>
+              <p class="bold">Les Mousquetaires’ Challenges</p>
+              <p>
+                Need to provide training to users and administrators following the acquisition of a new ATS. Rhaegal, after successfully implemented SmartRecruiters, ensured that future users would master the tool.
+              </p>
+            </div>
+            <div>
+              <p class="bold">Solution put in place by Rhaegal</p>
+              <p>
+                Transfer knowledge webinars for point-of-sale recruiters and administrators - in charge of user support. Edition of 10 tutorial videos to manage the essential functions Creation of a certification quiz to ensure that users have fully familiarised themselves with the tool
+              </p>
+            </div>
+            <div>
+              <p class="bold">Outcome</p>
+              <p>
+                Average score in the quiz : Recruiters ready to use the tool as soon as it was deployed Assurance of knowledge that lasts thanks to the tutorial videos Fully autonomous administrators to support users in stores
+              </p>
+            </div>
+            <button>
+                Click here
+            </button>
+          </article>
+        </section>
+      </section>
+    </a>
+       <a id="veolia">
+      <section>
+        <article class="purple-container">
+          <div id="blue-back">
+            <img src="../assets/image/veolia.png" alt="logo-veolia" />
+          </div>
+        </article>
+        <section class="border-orange">
+          <article class="white-content">
+            <div>
+              <p class="bold">Veolia’s Challenges</p>
+              <p>
+                The ATS changeover process has finally taken place in 2021.Veolia has officially selected SmartRecruiters as the ATS of choice to join its complex HRIS environment and become an integral part of the daily lives of hundreds of recruiters and a new way of interacting with thousands of candidates.
+              </p>
+            </div>
+            <article id="column">
+            <div>
+              <p class="bold">Outcome</p>
+              <p>
+                7 months between signing Veolia/SmartRecruiters and going live Smooth transition between both ATS New innovation opportunities for Veolia and Rhaegal thanks to the new recruitment system Attracting more candidates through the interactive map Strengthened relationship with Veolia
+              </p>
+            </div>
+            <div>
+              <p class="bold">History</p>
+              <p>
+               In 2017, Veolia has been using Taleo for +10 years. Taleo's configuration was no longer suitable for their recruitment needs, and no one who had the technical skills to remedy this. At this time, and until 2020, Rhaegal leveraged its expertise to reposition Taleo as an asset within the recruitment processes.
+              </p>
+            </div>
+
+            </article>
+            <div>
+              <p class="bold">Solution put in place by Rhaegal</p>
+              <p>
+                Form4Smart: allows the automatic sending of a form to candidates
+                to collect sensitive data. It is possible to specifically
+                configure the format of the candidate's response fields in order
+                to limit the risk of clumsy mistakes.
+              </p>
+            </div>
+            <button>
+                Click here
+            </button>
+          </article>
+        </section>
+      </section>
+    </a>
   </div>
 </template>
 
